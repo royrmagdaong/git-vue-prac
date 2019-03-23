98b505eb5371541486727f008c84502cb0329230
iamroy <royrmagdaong@gmail.com>
2019-03-23 13:19:49 +0800

    v-bind testing

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ new Vue({
   el:'#vue-app',
   data:{
     name:'Roy',
-    time: 'morning'
+    time: 'morning',
+    website: 'https://youtube.com',
+    age: 30,
+  },
+  methods:{
+    greet: function(time){
+      return 'Good '+time +' '+this.name;
+    },
+    addAge: function(age){
+      this.age +=age;
+    },
+    substractAge: function(age){
+      this.age -=age;
+    }
   }
 });
